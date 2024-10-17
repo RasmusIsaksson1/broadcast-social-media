e935ea8d2fa8e150086c0847ff15c0f3b463513d
RasmusIsaksson1 <Rasmus9927@gmail.com>
2024-10-17 09:43:14 +0200

    Added search functionality

---
+++ BroadcastSocialMedia/BroadcastSocialMedia/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using BroadcastSocialMedia.Data;
+using BroadcastSocialMedia.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BroadcastSocialMedia.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public UsersController(ApplicationDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+
+        public async Task<IActionResult> Index(UsersIndexViewModel viewModel)
+        {
+            if(viewModel.Search != null)
+            {
+                var users = await _dbcontext.Users.Where(u => u.Name.Contains(viewModel.Search))
+                                .ToListAsync();
+                viewModel.Result = users;
+            }
+            
+
+            return View(viewModel);
+        }
+    }
+}
